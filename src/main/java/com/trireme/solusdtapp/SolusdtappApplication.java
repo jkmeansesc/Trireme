ffd876aa6b0fb8f69106dee3ffa44a576551ba43
jkmeansesc <373441770@qq.com>
2024-09-28 21:49:52 +0100

    initial commit

---
+++ src/main/java/com/trireme/solusdtapp/SolusdtappApplication.java
@@ -0,0 +1,13 @@
+package com.trireme.solusdtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolusdtappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolusdtappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trireme/solusdtapp/SolusdtappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trireme.solusdtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolusdtappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
